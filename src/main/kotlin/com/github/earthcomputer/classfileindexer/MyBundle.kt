ee28f93882492e0b1b24ae271ddc6fb1ca7c4f8b
GitHub Action <action@github.com>
2021-07-27 17:47:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/earthcomputer/classfileindexer/MyBundle.kt
+++ src/main/kotlin/com/github/earthcomputer/classfileindexer/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.earthcomputer.classfileindexer
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/earthcomputer/classfileindexer/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.earthcomputer.classfileindexer.services
+
+import com.github.earthcomputer.classfileindexer.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
