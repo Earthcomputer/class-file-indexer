4971e4f52cdf6c39abf4e530f65f02e823f36d5a
Joe <burtonjae@hotmail.co.uk>
2021-08-06 23:02:42 +0100

    Revert constant pool stuff... the API is just not ready for it

--- src/main/kotlin/net/earthcomputer/classfileindexer/ClassFileIndexExtension.kt
+++ src/main/kotlin/net/earthcomputer/classfileindexer/ClassFileIndexExtension.kt
@@ -1,32 +1,20 @@
 package net.earthcomputer.classfileindexer
 
 import com.intellij.ide.highlighter.JavaClassFileType
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.util.indexing.CustomImplementationFileBasedIndexExtension
 import com.intellij.util.indexing.DataIndexer
 import com.intellij.util.indexing.DefaultFileTypeSpecificInputFilter
-import com.intellij.util.indexing.FileBasedIndex
 import com.intellij.util.indexing.FileBasedIndexExtension
 import com.intellij.util.indexing.FileContent
 import com.intellij.util.indexing.ID
-import com.intellij.util.indexing.IndexInfrastructure
-import com.intellij.util.indexing.impl.storage.VfsAwareIndexStorageLayout
-import com.intellij.util.indexing.impl.storage.VfsAwareMapReduceIndex
 import com.intellij.util.io.DataExternalizer
 import com.intellij.util.io.DataInputOutputUtil
-import com.intellij.util.io.IOUtil
 import com.intellij.util.io.KeyDescriptor
-import com.intellij.util.io.PersistentStringEnumerator
-import com.intellij.util.io.StorageLockContext
 import net.earthcomputer.classindexfinder.libs.org.objectweb.asm.ClassReader
 import java.io.DataInput
 import java.io.DataOutput
-import java.io.IOException
-import java.nio.file.Path
 
 class ClassFileIndexExtension :
-    FileBasedIndexExtension<String, Map<BinaryIndexKey, Map<String, Int>>>(),
-    CustomImplementationFileBasedIndexExtension<String, Map<BinaryIndexKey, Map<String, Int>>> {
+    FileBasedIndexExtension<String, Map<BinaryIndexKey, Map<String, Int>>>() {
     override fun getName() = INDEX_ID
 
     override fun getIndexer() = DataIndexer<String, Map<BinaryIndexKey, Map<String, Int>>, FileContent> { content ->
@@ -78,69 +66,72 @@ class ClassFileIndexExtension :
         }
     }
 
-    override fun getVersion() = 3
+    override fun getVersion() = 4
 
     override fun getInputFilter() = DefaultFileTypeSpecificInputFilter(JavaClassFileType.INSTANCE)
 
     override fun dependsOnFileContent() = true
 
     companion object {
-        private val LOGGER = Logger.getInstance(ClassFileIndexExtension::class.java)
+//        private val LOGGER = Logger.getInstance(ClassFileIndexExtension::class.java)
         val INDEX_ID = ID.create<String, Map<BinaryIndexKey, Map<String, Int>>>("classfileindexer.index")
-        private const val ENUMERATOR_INITIAL_SIZE = 1024 * 4
+//        private const val ENUMERATOR_INITIAL_SIZE = 1024 * 4
     }
 
-    private val enumeratorPath: Path = IndexInfrastructure.getIndexRootDir(INDEX_ID).resolve("classfileindexer.constpool")
-    private var enumerator = createEnumerator()
-
-    private fun createEnumerator(): PersistentStringEnumerator {
-        @Suppress("UnstableApiUsage")
-        return PersistentStringEnumerator(enumeratorPath, ENUMERATOR_INITIAL_SIZE, true, StorageLockContext(true))
-    }
-
-    private fun recreateEnumerator() {
-        IOUtil.closeSafe(LOGGER, enumerator)
-        IOUtil.deleteAllFilesStartingWith(enumeratorPath.toFile())
-        enumerator = createEnumerator()
-    }
+    // TODO: when all this becomes stable API...
+//    private val enumeratorPath: Path = IndexInfrastructure.getIndexRootDir(INDEX_ID).resolve("classfileindexer.constpool")
+//    private var enumerator = createEnumerator()
+//
+//    private fun createEnumerator(): PersistentStringEnumerator {
+//        @Suppress("UnstableApiUsage")
+//        return PersistentStringEnumerator(enumeratorPath, ENUMERATOR_INITIAL_SIZE, true, StorageLockContext(true))
+//    }
+//
+//    private fun recreateEnumerator() {
+//        IOUtil.closeSafe(LOGGER, enumerator)
+//        IOUtil.deleteAllFilesStartingWith(enumeratorPath.toFile())
+//        enumerator = createEnumerator()
+//    }
 
     private fun readString(input: DataInput): String {
-        return enumerator.valueOf(DataInputOutputUtil.readINT(input))?.intern()
-            ?: throw IOException("Invalid enumerated string")
+        return input.readUTF()
+//        return enumerator.valueOf(DataInputOutputUtil.readINT(input))?.intern()
+//            ?: throw IOException("Invalid enumerated string")
     }
 
     @Suppress("TooGenericExceptionCaught")
     private fun writeString(output: DataOutput, value: String) {
-        try {
-            DataInputOutputUtil.writeINT(output, enumerator.enumerate(value))
-        } catch (e: Throwable) {
-            recreateEnumerator()
-            FileBasedIndex.getInstance().requestRebuild(INDEX_ID, e)
-            throw e
-        }
+        output.writeUTF(value)
+//        try {
+//            DataInputOutputUtil.writeINT(output, enumerator.enumerate(value))
+//        } catch (e: Throwable) {
+//            recreateEnumerator()
+//            FileBasedIndex.getInstance().requestRebuild(INDEX_ID, e)
+//            throw e
+//        }
     }
 
-    @Suppress("UnstableApiUsage")
-    override fun createIndexImplementation(
-        extension: FileBasedIndexExtension<String, Map<BinaryIndexKey, Map<String, Int>>>,
-        indexStorageLayout: VfsAwareIndexStorageLayout<String, Map<BinaryIndexKey, Map<String, Int>>>
-    ) = object : VfsAwareMapReduceIndex<String, Map<BinaryIndexKey, Map<String, Int>>>(extension, indexStorageLayout, null) {
-        override fun doClear() {
-            super.doClear()
-            recreateEnumerator()
-        }
-
-        override fun doFlush() {
-            super.doFlush()
-            enumerator.force()
-        }
-
-        override fun doDispose() {
-            try {
-                super.doDispose()
-            } finally {
-                IOUtil.closeSafe(LOGGER, enumerator)
-            }
-        }
-    }
+//    @Suppress("UnstableApiUsage")
+//    override fun createIndexImplementation(
+//        extension: FileBasedIndexExtension<String, Map<BinaryIndexKey, Map<String, Int>>>,
+//        indexStorageLayout: VfsAwareIndexStorageLayout<String, Map<BinaryIndexKey, Map<String, Int>>>
+//    ) = object : VfsAwareMapReduceIndex<String, Map<BinaryIndexKey, Map<String, Int>>>(extension, indexStorageLayout, null) {
+//        override fun doClear() {
+//            super.doClear()
+//            recreateEnumerator()
+//        }
+//
+//        override fun doFlush() {
+//            super.doFlush()
+//            enumerator.force()
+//        }
+//
+//        override fun doDispose() {
+//            try {
+//                super.doDispose()
+//            } finally {
+//                IOUtil.closeSafe(LOGGER, enumerator)
+//            }
+//        }
+//    }
 }
